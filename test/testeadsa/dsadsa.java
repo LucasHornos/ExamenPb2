7e55bb8bfbd9ed3837c77608e97ad1abaf2d83b7
KevinLlombart <kevin.llombart00@gmail.com>
2023-11-08 18:46:04 -0300

    agrego archivo

---
+++ test/testeadsa/dsadsa.java
@@ -0,0 +1,15 @@
+package testeadsa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class dsadsa {
+
+	@Test
+	public void test() {
+		Integer dsasa = 0;
+		assertEquals(dsasa, (Integer)0);
+	}
+	
+}
